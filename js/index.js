d4a5b49b586cfe512041a28ba91b668334b3054b
ACarrill42 <austincarrill@gmail.com>
2020-10-30 21:45:00 -0500

    finished 2,3,4 listeners

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 // Your code goes here
 const logoClick = document.querySelector('.logo-heading');
-logoClick.addEventListener('click', event => event.target.style.color = 'green');
+logoClick.addEventListener('click', event => {
+    event.target.style.color = 'green'
+});
 
+const navBack = document.querySelector('.nav');
+navBack.addEventListener('mouseenter', event => {
+    event.target.style.background = 'pink'
+});
+
+const navReturn = document.querySelector('.nav');
+navReturn.addEventListener('mouseleave', event => {
+    event.target.style.background = 'white'
+});
+
+const form = document.querySelector('form');
+form.addEventListener('reset');
