63de5ee6101bc4c90e8c6fc6e681515602c87897
ACarrill42 <austincarrill@gmail.com>
2021-01-15 14:54:08 -0600

    added 4 listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+//click
+document.querySelectorAll(".btn").forEach(item => {
+    item.addEventListener("click", e => {
+        alert("Thank you!")
+    })
+})
+
+//contextmenu
+document.addEventListener('contextmenu', e => {
+    e.preventDefault();
+})
+
+//input
+const input = document.querySelector('input');
+const log = document.querySelector('#values');
+
+input.addEventListener('input', e => {
+    log.textContent = e.target.value
+})
+
+//keydown 
+document.addEventListener('keydown', e => {
+    console.log(`${e.code}`)
+})
